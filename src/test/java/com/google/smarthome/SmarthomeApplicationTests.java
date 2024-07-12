eec0dce2728fdf339b710b48489c93ddcab223a7
Kim971202 <kimyohan971202@gmail.com>
2024-07-12 16:14:55 +0900

    Redis Test 추가

--- src/test/java/com/google/smarthome/SmarthomeApplicationTests.java
+++ src/test/java/com/google/smarthome/SmarthomeApplicationTests.java
@@ -1,10 +1,16 @@
 package com.google.smarthome;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.redis.core.RedisTemplate;
 
 @SpringBootTest
 class SmarthomeApplicationTests {
+
+	@Autowired
+	private RedisTemplate<String, Object> redisTemplate;
+
 
 	@Test
 	void contextLoads() {
