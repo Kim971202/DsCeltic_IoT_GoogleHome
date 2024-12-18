951a3666c1b19c79a85588db6967aa74fae1681f
Kim971202 <kimyohan971202@gmail.com>
2024-12-18 16:50:44 +0900

    handleQuery 수정중

--- src/main/java/com/google/smarthome/service/FulfillmentService.java
+++ src/main/java/com/google/smarthome/service/FulfillmentService.java
@@ -516,7 +516,7 @@ public class FulfillmentService {
                 device.put("traits", new JSONArray()
                         .put("action.devices.traits.OnOff")
                         .put("action.devices.traits.TemperatureSetting")
-                        .put("action.devices.traits.TemperatureControl")
+//                        .put("action.devices.traits.TemperatureControl")
                         .put("action.devices.traits.Modes"));
             } else if (modelCode.equals("DCR-47/WF")) {
                 deviceType = "action.devices.types.FAN";
@@ -702,14 +702,9 @@ public class FulfillmentService {
             boolean deviceOnOff = "on".equals(deviceStatus.getPowrStatus());
             deviceState.put("on", deviceOnOff);
             deviceState.put("online", true);
-//            deviceState.put("currentModeSettings", currentModeSettings);
-            deviceState.put("activeThermostatMode", "none");
+            deviceState.put("currentModeSettings", currentModeSettings);
             deviceState.put("thermostatMode", deviceOnOff ? "heat" : "off");
-//            deviceState.put("temperatureSetpointCelsius", String.format("%.1f", Double.parseDouble(deviceStatus.getTempStatus())));
-//            deviceState.put("temperatureAmbientCelsius",  String.format("%.1f", 25.0));
-//            deviceState.put("thermostatTemperatureSetpoint", String.format("%.1f", Double.parseDouble(deviceStatus.getTempStatus())));
-            deviceState.put("thermostatTemperatureSetpointHigh", String.format("%.1f", 26.0));
-            deviceState.put("thermostatTemperatureSetpointLow", String.format("%.1f", 22.0));
+            deviceState.put("thermostatTemperatureSetpoint", String.format("%.1f", Double.parseDouble(deviceStatus.getTempStatus())));
             deviceState.put("thermostatTemperatureAmbient",  String.format("%.1f", 25.0));
 
             devices.put(deviceId, deviceState);
